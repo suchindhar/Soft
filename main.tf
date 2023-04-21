867ff191e53f64062e14a1c05dc9277b34841e7a
suchindhar <56792748+suchindhar@users.noreply.github.com>
2023-04-21 14:39:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # terraform block 
 terraform {
-  required_version = "~>1.3.0"
+ 
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.60.0"
+      
     }
   }
 }
